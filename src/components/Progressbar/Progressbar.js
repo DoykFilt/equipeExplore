038e1cf4e858430fa2c799344bfb8773e3af3db3
Hilleriteau Jean <jhi@live.fr>
2019-02-13 15:59:17 +0100

    déplacement avec barre de progression

--- src/components/Progressbar/Progressbar.js
+++ src/components/Progressbar/Progressbar.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './Progressbar.css';
+import move from '../../js/move';
 
 export default class Progressbar extends Component {
 
     render() {
+        
+        function click_handler(event)
+        {
+            const ending_offset = parseInt(document.getElementById('board').offsetWidth * -1 + document.documentElement.clientWidth, 10);
+            var boardWidth = parseInt(document.getElementById('board').offsetWidth, 10);
+            const position = (event.clientX / document.documentElement.clientWidth * ending_offset ) + document.documentElement.clientWidth;
+            console.log(`mouse posistion ${event.clientX}, board_width ${boardWidth}, ending_offset ${ending_offset}, position ${position}, client_width ${document.documentElement.clientWidth}`);
+            
 
+            // var progression = (Math.abs(offset) + document.documentElement.clientWidth) * 100 / boardWidth;
+            move.to(position);
+        }
         return (
-            <div className="Progressbar">
+            <div className="Progressbar" id="progressbar_container" onClick={click_handler}>
                 <div className="Progressbar__value" id="progressbar"></div>
             </div>
         );
